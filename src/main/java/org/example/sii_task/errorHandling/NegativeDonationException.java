35ffd234d406e64dd1010f397a6e5728225fcade
agadz <aga.dziabor@gmail.com>
2025-05-14 18:59:15 +0200

    Donations cannot be negative or 0 now

---
+++ src/main/java/org/example/sii_task/errorHandling/NegativeDonationException.java
@@ -0,0 +1,7 @@
+package org.example.sii_task.errorHandling;
+
+public class NegativeDonationException extends RuntimeException {
+  public NegativeDonationException(String message) {
+    super(message);
+  }
+}
